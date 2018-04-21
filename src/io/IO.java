f770131977ac9bb61a5423ac2b3a073963e56f57
midoMughrabi <mydo.mughrabi@gmail.com>
2018-04-21 21:57:25 +0200

    mavenising, adding more structure

---
+++ src/io/IO.java
@@ -0,0 +1,17 @@
+package io;
+
+import java.util.List;
+import model.Person;
+
+public class IO {
+
+	public static List<Person> readPersons()
+	{
+		return null;
+	}
+	
+	public static void writePersons(List<Person> persons)
+	{
+		
+	}
+}
